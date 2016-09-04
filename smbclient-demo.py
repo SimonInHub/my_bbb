be1f53e9ee000b71768da33dcdde6e1359154b56
yangxiyuan <yangxiyuan@ihealthlabs.com.cn>
2016-09-04 13:44:20 +0800

    First commit

---
+++ smbclient-demo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import smbclient
+smb = smbclient.SambaClient(server="192.168.1.6",share="招聘简历",username='13242', password='snahko', domain='andon.com')
+file_list=smb.listdir(u"/iHealth/8月/数据处理工程师/")
+for item in file_list:
+	print item
+	smb.download(u"/iHealth/8月/数据处理工程师/"+item,u"/media/USBDISK/"+item)
